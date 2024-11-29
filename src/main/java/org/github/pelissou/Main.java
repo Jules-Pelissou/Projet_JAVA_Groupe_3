304267d3bac2f3ff227f31ab60b5df169ac02c9b
eferrier81 <enzoferrier81120@gmail.com>
2024-11-29 09:52:16 +0100

    TODO : Main

--- src/main/java/org/github/pelissou/Main.java
+++ src/main/java/org/github/pelissou/Main.java
@@ -4,8 +4,29 @@ package org.github.pelissou;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /*
+         * 3.1 Exemple de test
+         * En guise de test initial, vous pourrez tenter de simuler la propagation d’un
+         * virus aux
+         * caractéristiques suivantes :
+         * •Taux de transmission :
+         * – Sensibles : 0,3
+         * – Neutres : 0,2
+         * – Résistants : 0,1
+         * •Période d’incubation : 2 cycles
+         * •Probabilité de guérison : 0,4
+         * •Probabilité de décès : 0,1
+         * •Distance maximale de transmission (dmax) : 10 km
+         * •Facteur initial de probabilité de transmission (p0) : 0,5
+         * La propagation se fait dans une population de 100 individus, répartis
+         * aléatoirement
+         * dans un espace en deux dimensions (10km sur 10km), non vaccinés, avec une
+         * sensibilité
+         * neutre au virus, et n’ayant pas de comportement préventif particulier sur une
+         * période de
+         * 10 cycles. Vous pourrez commencer le premier cycle avec 20% de la population
+         * atteinte
+         * du virus.
+         */
     }
 }

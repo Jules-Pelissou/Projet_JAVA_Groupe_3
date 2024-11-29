ed39bb683cf76fffd4708c4bc537e049959088d8
eferrier81 <enzoferrier81120@gmail.com>
2024-11-29 12:14:02 +0100

    Supression tauxTransmission

--- src/main/java/org/github/pelissou/Variant.java
+++ src/main/java/org/github/pelissou/Variant.java
@@ -1,12 +1,8 @@
 package org.github.pelissou;
 public class Variant extends Epidemie{
     public Variant(float tauxTransmissionIni, int periodeIncubation, float probaGuerison, float probaDeces, double distanceMax, float tauxTransmission, int cycles) {
-        super(tauxTransmissionIni, periodeIncubation, probaGuerison, probaDeces, distanceMax, tauxTransmission, cycles);
+        super(tauxTransmissionIni, periodeIncubation, probaGuerison, probaDeces, distanceMax, cycles);
         this.tauxTransmissionIni = tauxTransmissionIni;
         this.periodeIncubation = periodeIncubation;
-    }
-
-    public float getTauxTransmission() {
-        return super.getTauxTransmission();
     }
 }

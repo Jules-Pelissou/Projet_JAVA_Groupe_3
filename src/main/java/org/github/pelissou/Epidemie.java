920bcdb4f1f400e0c8f3a2888b5ef3ada737d4c8
eferrier81 <enzoferrier81120@gmail.com>
2024-11-29 11:14:16 +0100

    Cycles passés en static

--- src/main/java/org/github/pelissou/Epidemie.java
+++ src/main/java/org/github/pelissou/Epidemie.java
@@ -6,17 +6,22 @@ public class Epidemie {
     private float probaDeces;
     private double distanceMax;
     private float tauxTransmission;
+    private static int cycles;
 
-    public Epidemie(float tauxTransmissionIni, int periodeIncubation, float probaGuerison, float probaDeces, double distanceMax, float tauxTransmission) {
+    public Epidemie(float tauxTransmissionIni, int periodeIncubation, float probaGuerison, float probaDeces, double distanceMax, float tauxTransmission, int cycles) {
         this.tauxTransmission = tauxTransmissionIni;
         this.periodeIncubation = periodeIncubation;
         this.probaGuerison = probaGuerison;
         this.probaDeces = probaDeces;
         this.distanceMax = distanceMax;
-        
+        this.cycles = cycles;
     } 
     
     public float getTauxTransmission() {
         return tauxTransmission;
+    }
+
+    public static int getCycles() {
+        return cycles;
     }
 }

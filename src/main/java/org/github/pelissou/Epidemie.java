5c55c0dd9069c1e77bf282212fd1b28f5e637ab1
AlexisDechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-11-29 10:46:30 +0100

    Modif Epidemie.java pour correspondre à l'UML

--- src/main/java/org/github/pelissou/Epidemie.java
+++ src/main/java/org/github/pelissou/Epidemie.java
@@ -1,19 +1,19 @@
 package org.github.pelissou;
 public class Epidemie {
-    private float tauxTransmission;
+    private float tauxTransmissionIni;
     private int periodeIncubation;
     private float probaGuerison;
     private float probaDeces;
     private double distanceMax;
-    private float probaComtamination;
+    private float tauxTransmission;
 
-    public Epidemie(float tauxTransmission, int periodeIncubation, float probaGuerison, float probaDeces, double distanceMax, float probaComtamination) {
-        this.tauxTransmission = tauxTransmission;
+    public Epidemie(float tauxTransmissionIni, int periodeIncubation, float probaGuerison, float probaDeces, double distanceMax, float tauxTransmission) {
+        this.tauxTransmission = tauxTransmissionIni;
         this.periodeIncubation = periodeIncubation;
         this.probaGuerison = probaGuerison;
         this.probaDeces = probaDeces;
         this.distanceMax = distanceMax;
-        this.probaComtamination = probaComtamination;
+        
     } 
     
     public float getTauxTransmission() {

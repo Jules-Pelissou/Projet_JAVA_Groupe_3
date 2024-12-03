67eca97c96903bd2c6d618c366694d48eef6a1a1
AlexisDechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-12-03 17:58:17 +0100

    Changement du nom Epidemie -> Maladie

--- src/main/java/org/github/pelissou/Variant.java
+++ src/main/java/org/github/pelissou/Variant.java
@@ -1,5 +1,5 @@
 package org.github.pelissou;
-public class Variant extends Epidemie{
+public class Variant extends Maladie{
     public Variant(float tauxTransmissionIni, int periodeIncubation, float probaGuerison, float probaDeces, double distanceMax, float tauxTransmission, int cycles) {
         super(tauxTransmissionIni, periodeIncubation, probaGuerison, probaDeces, distanceMax, cycles);
         this.tauxTransmissionIni = tauxTransmissionIni;

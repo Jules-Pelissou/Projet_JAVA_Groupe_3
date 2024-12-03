6565d70e84ebb0f2c3de25d1a34c4210a65fe3c6
AlexisDechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-12-03 17:59:29 +0100

    Ajout de Cycle.java

---
+++ src/main/java/org/github/pelissou/Cycle.java
@@ -0,0 +1,11 @@
+package org.github.pelissou;
+
+public class Cycle {
+    private int numero;
+    private int duree;
+
+    public Cycle(int numeroDuCycle,int duree) {
+        this.numero = numeroDuCycle;
+        this.duree = duree;
+    }
+}
